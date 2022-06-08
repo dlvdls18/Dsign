021270a02d40935408e969a90abf46df30201966
Delve Delos Santos Jr <106716453+dlvdls18@users.noreply.github.com>
2022-06-08 09:31:39 +0800

    update dsign.js

--- src/dsign.js
+++ src/dsign.js
@@ -11,15 +11,15 @@ function Dsign() {
     var sr = style.replace(/(A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z)/g, "-$1").toLowerCase();
     document.querySelectorAll(`*`).forEach(function(el) {
       (el.getAttribute("class") || "").split(" ").forEach(function(c) {
-        if(c.startsWith(style + "-")) {
-          var cv = c.replace(style + "-", "");
+        if(c.startsWith(sr + "-")) {
+          var cv = c.replace(sr + "-", "");
           el.style[style] = cv;
         }
       });
     });
     document.querySelectorAll("[" + sr + "]").forEach(function(el) {
       el.style[style] = el.getAttribute(sr);
-    }); 
+    });
   }
 }
 
