8a688ebecf60b26ac4eafeed63cde65769764aa5
Delve Delos Santos Jr <106716453+dlvdls18@users.noreply.github.com>
2022-06-09 11:04:33 +0800

    update dsign.js

--- src/dsign.js
+++ src/dsign.js
@@ -6,13 +6,40 @@
  *
  */
 
+var dsign = {};
+
 function Dsign() {
-  var m = document.body.innerHTML.matchAll(/<!---@\sdsign\s:\s(.+)\s:\s(.+)\s@--->/gm);
-  console.log(m);
+  document.querySelectorAll("dsign").forEach(function(el) {
+    el.style.display = "none";
+    if(el.innerText != "") dsign[el.getAttribute("name")] =  [0, el.innerText.split(" ")];
+    else {
+      var attrs = {};
+      for(var i in el.attributes) {
+        if(!isNaN(parseInt(i)) && i != "name") {
+          var attr = el.attributes[i];
+          if(attr.name != "name") attrs[attr.name] = attr.value;
+        }
+      }
+      dsign[el.getAttribute("name")] =  [1, attrs];
+    }
+  });
+  for(var i in dsign) {
+    document.querySelectorAll(`[data-dsign="${i}"]`).forEach(function(el) {
+      if(dsign[i][0] == 0) dsign[i][1].forEach(function(item) {
+        if(!el.classList.contains(item)) el.classList.add(item);
+      });
+      else {
+        var attrs = dsign[i][1];
+        for(var name in attrs) {
+          if(name != "style") el.setAttribute(name, attrs[name]);
+        }
+      }
+    });
+  }
   for(var style in document.body.style) {
     var sr = style.replace(/(A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z)/g, "-$1").toLowerCase();
-    console.log(sr, style);
     document.querySelectorAll("*").forEach(function(el) {
+      if(el.localName == "dsign") return;
       (el.getAttribute("class") || "").split(" ").forEach(function(c) {
         if(c.startsWith(sr + "-")) {
           var cv = c.replace(sr + "-", "");
@@ -21,6 +48,7 @@ function Dsign() {
       });
     });
     document.querySelectorAll("[" + sr + "]").forEach(function(el) {
+      if(el.localName == "dsign") return;
       el.style[style] = el.getAttribute(sr);
     });
   }
