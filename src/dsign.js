5f0daeedd86d97152a37f48bcd47b1923c6b5262
Delve Delos Santos Jr <106716453+dlvdls18@users.noreply.github.com>
2022-06-12 11:39:20 +0800

    update dsign.js

--- src/dsign.js
+++ src/dsign.js
@@ -11,7 +11,7 @@ function Dsign(conf) {
     var s = conf[el.getAttribute("data-dsign")];
     if(!s) return;
     if(s.class) {
-      el.className += s.class.join(" ");
+      el.className = s.class.join(" ");
     }
     if(s.attr) {
       for(var key in s.attr) {
